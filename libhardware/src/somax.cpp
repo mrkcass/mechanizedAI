2eb18d8c406a9dce9243872733ba5b933b695e00
mcass <mrkcass@gmail.com>
2018-10-08 22:23:47 -0700

    add conversion functions. support multiple mainboards

--- libhardware/src/somax.cpp
+++ libhardware/src/somax.cpp
@@ -1,11 +1,32 @@
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// author: mark cass
+// project: somax personal AI
+// project url: https://mechanizedai.com
+// license: open source and free for all uses without encumbrance.
+//
+// FILE: somax.c
+// DESCRIPTION: Somax constants and utility functions.
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 
 #include "somax.h"
 
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//CONSTANTS
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 #define SOMAX_PRINTOPTION_CONFIG 0x01
 
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//DATA STRUCTURES
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 struct SOMAXDATA
 {
    char name[256];
@@ -14,7 +35,48 @@ struct SOMAXDATA
    char resources[256];
 };
 
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//DATA
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 static struct SOMAXDATA somax_data;
+
+static char loglevel_strings[NUM_SOMAX_LOG_LEVELS+1][SOMAX_LOG_FORMAT_MAX_LENGTH + 1] =
+{
+   "NULL",
+   "INFO   :",
+   "WARNING:",
+   "ERROR  :",
+};
+
+static char mainboardid_strings[SOMAX_NUM_MAINBOARDS+1][SOMAX_MAINBOARDID_STR_MAX_LENGTH + 1] =
+{
+   "UNKNOWN",
+   "EDISON",
+   "HIKEY960",
+};
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//PUBLIC FUNCTIONS
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+somax_mainboard_id_t somax_mainboard_id()
+{
+   return SOMAX_MAINBOARDID_EDISON;
+}
+
+char* somax_mainboard_id_to_string(somax_mainboard_id_t id)
+{
+   if (id < 0 || id > SOMAX_NUM_MAINBOARDS)
+   {
+      somax_log_add(SOMAX_LOG_ERR, "somax mainboard id string request for id (%d)out of range", id);
+      return mainboardid_strings[SOMAX_MAINBOARDID_UNKNOWN];
+   }
+   return mainboardid_strings[id];
+}
+
 
 void somax_data_init(const char name[], const char sclass[], const char version[], const char resources[])
 {
@@ -64,8 +126,21 @@ bool somax_commandline_has_option(int argc, char *argv[], const char option_name
 
 void somax_log_add(int log_level, const char *fmt, ...)
 {
+   char log_fmt[SOMAX_LOG_FORMAT_MAX_LENGTH + SOMAX_LOG_LEVEL_STRING_MAX_LEN + 1];
+
    va_list args;
    va_start(args, fmt);
-   vprintf(fmt, args);
+   snprintf(log_fmt, SOMAX_LOG_FORMAT_MAX_LENGTH + SOMAX_LOG_LEVEL_STRING_MAX_LEN + 1, "%s %s\n", loglevel_strings[log_level], fmt);
+   vprintf(log_fmt, args);
    va_end(args);
+}
+
+float somax_convert_celsius_to_farenheit(float temp_celsius)
+{
+   return (temp_celsius * (9.0 / 5.0)) + 32.0;
+}
+
+float somax_convert_farenheit_to_celsius(float temp_farenheit)
+{
+   return (temp_farenheit - 32.0) * (5.0 / 9.0) + 32;
 }
--- libhardware/src/somax.h
+++ libhardware/src/somax.h
@@ -1,5 +1,16 @@
 #ifndef __somax_h__
 #define __somax_h__
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// author: mark cass
+// project: somax personal AI
+// project url: https://mechanizedai.com
+// license: open source and free for all uses without encumbrance.
+//
+// FILE: somax.h
+// DESCRIPTION: Somax constants and utility functions.
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 // #ifndef SOMAX_NAME
 // #define SOMAX_NAME "UNKNOWN"
@@ -19,6 +30,8 @@
 
 #define SOMAX_PRINTOPTION_CONFIG 0x01
 
+#define MAX_STR_DEVICEID_LEN 32
+
 void somax_data_init(const char name[], const char sclass[], const char version[], const char resources[]);
 
 void somax_print_config(int print_options);
@@ -26,10 +39,28 @@ void somax_print_config(int print_options);
 bool somax_commandline_has_option(int argc, char *argv[], const char option_name[]);
 bool somax_commandline_options_handler(int argc, char *argv[]);
 
-#define SOMAX_LOG_INFO 1
-#define SOMAX_LOG_WARN 2
-#define SOMAX_LOG_ERR  3
+typedef int somax_mainboard_id_t;
+#define SOMAX_NUM_MAINBOARDS           2
+#define SOMAX_MAINBOARDID_UNKNOWN      0
+#define SOMAX_MAINBOARDID_EDISON       1
+#define SOMAX_MAINBOARDID_HIKEY960     2
+#define SOMAX_MAINBOARDID_STR_MAX_LENGTH 16
+somax_mainboard_id_t somax_mainboard_id();
+char* somax_mainboard_id_to_string(somax_mainboard_id_t id);
+
+#define NUM_SOMAX_LOG_LEVELS  3
+#define SOMAX_LOG_INFO        1
+#define SOMAX_LOG_WARN        2
+#define SOMAX_LOG_ERR         3
+
+//not including NULL terminator
+#define SOMAX_LOG_FORMAT_MAX_LENGTH 256
+//not including NULL terminator
+#define SOMAX_LOG_LEVEL_STRING_MAX_LEN 8
 
 void somax_log_add(int log_level, const char *fmt, ...);
+
+float somax_convert_celsius_to_farenheit(float temp_celsius);
+float somax_convert_farenheit_to_celsius(float temp_farenheit);
 
 #endif
