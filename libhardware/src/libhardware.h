78bca601a32636cfadedafce2a04baabbcfd9300
mcass <mrkcass@gmail.com>
2018-10-08 22:28:01 -0700

    add hardware init and configure abstraction

---
+++ libhardware/src/libhardware.h
@@ -0,0 +1,18 @@
+#ifndef __libhardware_h__
+#define __libhardware_h__
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// author: mark cass
+// project: somax personal AI
+// project url: https://mechanizedai.com
+// license: open source and free for all uses without encumbrance.
+//
+// FILE: i2c_interface.c
+// DESCRIPTION: libhardware common initialization and configuration.
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+void libhardware_init();
+void libhardware_deinit();
+
+#endif
